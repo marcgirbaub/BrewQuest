ab383a2576b7fae212736bb4789ac37c0b686c23
Marc Girbau <marc.girbau94@gmail.com>
2024-01-27 19:50:16 +0100

    Implement getBeers function

---
+++ src/api/getBeers/getBeers.ts
@@ -0,0 +1,22 @@
+import { AxiosResponse } from "axios";
+import apiClient from "../apiClient";
+import { beersEndpoint } from "../apiEndpoints";
+import { ApiBeersResponse } from "../../types/types";
+
+interface GetBeersParams {
+  beer_name: string;
+  brewed_before: string;
+}
+
+const getBeers = async (params?: GetBeersParams) => {
+  const beers: AxiosResponse<ApiBeersResponse> = await apiClient.get(
+    beersEndpoint,
+    {
+      params: params,
+    },
+  );
+
+  return beers.data;
+};
+
+export default getBeers;
