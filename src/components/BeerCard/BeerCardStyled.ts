ed320f7895791c6311e6a3191290fca6e84514ed
Marc Girbau <marc.girbau94@gmail.com>
2024-01-30 18:44:57 +0100

    Create BeerCard component and its main styles

---
+++ src/components/BeerCard/BeerCardStyled.ts
@@ -0,0 +1,48 @@
+import styled from "@emotion/styled";
+import { Paper } from "@mui/material";
+import { breakpoints } from "../../styles/breakpoints";
+
+const BeerCardStyled = styled(Paper)`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem;
+
+  @media only screen and (min-width: ${breakpoints.sm}px) {
+    flex-direction: row;
+  }
+
+  .beer-card {
+    &__image {
+      min-width: 100px;
+      display: flex;
+      justify-content: center;
+
+      & > img {
+        object-fit: contain;
+      }
+    }
+  }
+
+  .info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.5rem;
+
+    @media only screen and (min-width: ${breakpoints.sm}px) {
+      align-items: flex-start;
+    }
+
+    &__description {
+      display: -webkit-box;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      text-align: justify;
+    }
+  }
+`;
+
+export default BeerCardStyled;
